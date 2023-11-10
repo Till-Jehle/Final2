b4cca800e479c6345a44455bca7fc4ea382be23d
Till-Jehle <147993669+Till-Jehle@users.noreply.github.com>
2023-11-10 12:40:04 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AboutSection from "@/components/AboutSection"
 import HeroSection from "@/components/HeroSection"
-import ProjectsSection from "@/components/ProjectSection"
+import ProjectsSection from "@/components/ProjectsSection"
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 md:max-w-5xl">
