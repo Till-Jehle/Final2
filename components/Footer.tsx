928374322d227a09aeb242738fef28eac3f4fba1
Till-Jehle <147993669+Till-Jehle@users.noreply.github.com>
2023-11-10 12:47:59 +0100

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,69 +1,27 @@
 import React from "react"
-import Image from "next/image"
+import {
+} from "react-icons/ai"
 
-const skills = [
-  { skill: "HTML" },
-  { skill: "CSS" },
-  { skill: "JavaScript" },
-  { skill: "Design" },
-  { skill: "Perfektionismus" },
-  { skill: "Kreativ" },
-  { skill: "Motiviert" },
-  { skill: "Tailwind CSS"},
-]
-
-const AboutSection = () => {
+const Footer = () => {
   return (
-    <section id="about">
-      <div className="my-12 pb-12 md:pt-16 md:pb-48">
-        <h1 className="text-center font-bold text-4xl">
-          Über Mich
-          <hr className="w-6 h-1 mx-auto my-4 bg-green-400 border-0 rounded"></hr>
-        </h1>
+    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+      <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
+      <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
+          © 2023 Till Jehle<a href="/" className="hover:underline"></a>
+        </div>
+        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
+          <p className="text-white">Kontakt:</p>
 
-        <div className="flex flex-col space-y-10 items-stretch justify-center align-top px-6 md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
-          <div className="md:w-1/2 ">
-            <p>
-              Hi, mein name ist Till Jehhle und mein Ziel ist Web-developer
-              zu werden.
-            </p>
-            <br />
-            <p>
-              Ich bin 15 Jahre alt, in der 10. Klasse und mache dieses Jahr meinen Realschulabschluss.
-              Meine interesse an Web-Development habe ich letztes Jahr gefunden. Davor habe ich versucht meine ersten Videospiele
-              mit der Unreal Engine zu programieren. Angefangen mich mit Programiersprachen und Technik zu befassen
-              habe ich im Jahr 2020. 
-            </p>
-            <br />
-            <p>
-              Meine Hobbys außer programieren sind teilweise sehr unterschiedlich. Ich zeichne öfters an meinem Grafiktablet,
-              fotografiere und filme sehr gerne mit meinem Handy, mache sehr viel sport und Boxe ab und zu. 
-            </p>
-            <br />
-            <p>
-             Ich bin motiviert und versuche mich immer wieder mich zu verbessern, meine Stärken in der Schule sind: Mathe, Englisch,
-             Physik und Sport.
-            </p>
-          </div>
-          <div className="text-center md:w-1/2 md:text-left">
-            <h1 className="text-2xl font-bold mb-6">Meine Skills</h1>
-            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
-              {skills.map((item, idx) => {
-                return (
-                  <p
-                    key={idx}
-                    className="bg-neutral-600 px-4 py-2 mr-2 mt-2 rounded font-semibold text-white shadow-6xl shadow-green-400"
-                  >
-                    {item.skill}
-                  </p>
-                )
-              })}
-            </div>
-          </div>
+          <a href="mailto:till.jehle@gmail.com">
+          <svg class="h-8 w-8 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
+          
+          </a>
         </div>
       </div>
-    </section>
+    </footer>
   )
 }
 
-export default AboutSection
+export default Footer
