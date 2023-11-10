e7d598b826e282375f39c70408cd7bfa3a476cbe
Till-Jehle <147993669+Till-Jehle@users.noreply.github.com>
2023-11-10 11:40:45 +0100

    All files

---
+++ components/AboutSection.tsx
@@ -0,0 +1,69 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "Design" },
+  { skill: "Perfektionismus" },
+  { skill: "Kreativ" },
+  { skill: "Motiviert" },
+  { skill: "Tailwind CSS"},
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          Über Mich
+          <hr className="w-6 h-1 mx-auto my-4 bg-green-400 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top px-6 md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <p>
+              Hi, mein name ist Till Jehhle und mein Ziel ist Web-developer
+              zu werden.
+            </p>
+            <br />
+            <p>
+              Ich bin 15 Jahre alt, in der 10. Klasse und mache dieses Jahr meinen Realschulabschluss.
+              Meine interesse an Web-Development habe ich letztes Jahr gefunden. Davor habe ich versucht meine ersten Videospiele
+              mit der Unreal Engine zu programieren. Angefangen mich mit Programiersprachen und Technik zu befassen
+              habe ich im Jahr 2020. 
+            </p>
+            <br />
+            <p>
+              Meine Hobbys außer programieren sind teilweise sehr unterschiedlich. Ich zeichne öfters an meinem Grafiktablet,
+              fotografiere und filme sehr gerne mit meinem Handy, mache sehr viel sport und Boxe ab und zu. 
+            </p>
+            <br />
+            <p>
+             Ich bin motiviert und versuche mich immer wieder mich zu verbessern, meine Stärken in der Schule sind: Mathe, Englisch,
+             Physik und Sport.
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">Meine Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-neutral-600 px-4 py-2 mr-2 mt-2 rounded font-semibold text-white shadow-6xl shadow-green-400"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ components/HeroSection.tsx
@@ -0,0 +1,57 @@
+"use client" // this is a client component
+import React from "react"
+import Image from "next/image"
+import { Link } from "react-scroll/modules"
+import { HiArrowDown } from "react-icons/hi"
+
+const HeroSection = () => {
+  return (
+    <section id="home">
+      <div className="my-24 flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 md:flex-row md:space-x-4 md:text-left">
+        <div className="md:w-4/5 shadow-6xl shadow-green-400">
+          <Image
+            src="/headshot.png"
+            alt=""
+            width={1440}
+            height={1920}
+            className="justify-end"
+          />
+        </div>
+        <div className="md:mt-2 md:w-3/5">
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-6xl">Hi ich bin Till,</h1>
+          <p className="text-lg mt-4 mb-6 md:text-2xl">
+            mein Ziel ist es {" "}
+            <span className="font-semibold text-green-400">
+              Web-Developer{" "}
+            </span>
+            zu werden und meine coding und Web-Dev Skills auszubauen.
+          </p>
+          <Link
+            to="projects"
+            className="text-white font-semibold px-6 py-3 bg-neutral-600 rounded shadow brightness-90 transition duration-500 shadow-6xl shadow-green-400"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Projekte
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-row items-center text-center justify-center ">
+        <Link
+          to="about"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >
+        </Link>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
