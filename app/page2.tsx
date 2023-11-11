ea1b52601faaf4da2bcaa0452eda0d94f1e2f2c3
Till-Jehle <147993669+Till-Jehle@users.noreply.github.com>
2023-11-11 18:50:10 +0100

    Pagelink
    added a new page for the projects

---
+++ app/page2.tsx
@@ -0,0 +1,9 @@
+import WebsiteSection from "@/components/WebsiteSection"
+
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-3xl px-4 md:max-w-5xl">
+        <WebsiteSection />
+    </main>
+  )
+}
---
+++ components/WebsiteSection.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Image from "next/image"
+
+
+
+const WebsiteSection = () => {
+  return (
+    <div className="my-24 flex flex-col text-center items-center justify-center md:flex-row md:space-x-4 md:text-left">
+        <div className="md:w-4/5 shadow-6xl shadow-green-400">
+            <image
+            src="/headshot.png"
+            width={1440}
+            height={1920}
+            className="justify-end"
+            />
+
+        </div>
+    </div>
+  )
+}
+
+export default WebsiteSection
