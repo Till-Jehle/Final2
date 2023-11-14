f420f2346cdbba2d22f075bc2f56e90a3079fe76
Till-Jehle <147993669+Till-Jehle@users.noreply.github.com>
2023-11-14 17:34:50 +0100

    rollback

--- app/page2.tsx
+++
@@ -1,9 +0,0 @@
-import WebsiteSection from "@/components/WebsiteSection"
-
-export default function Home() {
-  return (
-    <main className="mx-auto max-w-3xl px-4 md:max-w-5xl">
-        <WebsiteSection />
-    </main>
-  )
-}
