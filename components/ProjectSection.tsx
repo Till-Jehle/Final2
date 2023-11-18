886fc186a107f3bc7e7b843e5ca9ec85fee95cfb
Tillmann - Dev <147993669+Till-Jehle@users.noreply.github.com>
2023-11-18 13:07:27 +0100

    Update ProjectSection.tsx

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -47,7 +47,7 @@ const ProjectsSection = () => {
                     </Link>
                   </div>
                   <div className="mt-8 md:w-1/2">
-                    <h1 className="text-4xl font-bold mb-6 text.white">{project.name}</h1>
+                    <h1 className="text-4xl font-bold mb-6 text-white">{project.name}</h1>
                     <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
                       {project.description}
                     </p>
